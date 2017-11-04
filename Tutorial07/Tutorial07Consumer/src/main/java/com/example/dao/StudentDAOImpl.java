1d569aa36fd16f4d982833be6581da83b58975b4
mayanggita <mayanggitaindra@gmail.com>
2017-11-04 17:10:20 +0700

    service consumer latihan 3

--- Tutorial07/Tutorial07Consumer/src/main/java/com/example/dao/StudentDAOImpl.java
+++ Tutorial07/Tutorial07Consumer/src/main/java/com/example/dao/StudentDAOImpl.java
@@ -22,7 +22,8 @@ public class StudentDAOImpl implements StudentDAO {
 
 	@Override
 	public List<StudentModel> selectAllStudents() {
-		return null;
+		List<StudentModel> students = restTemplate.getForObject("http://localhost:8080/rest/student/viewall", List.class);
+		return students;
 	}
 
 }
